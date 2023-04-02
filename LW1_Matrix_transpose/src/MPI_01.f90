3c6da6570645ab48249bcce4ecf626343fb55dbf
Vladimir Evdokimov <evladich2002@yandex.ru>
2023-04-03 00:28:37 +0500

    LW1 English comments

--- LW1_Matrix_transpose/src/MPI_01.f90
+++ LW1_Matrix_transpose/src/MPI_01.f90
@@ -7,17 +7,17 @@ program matrixTranspose
     real, allocatable :: matrix(:, :)
     integer :: err, nproc, myID
 
-    ! инициализация MPI
+    ! MPI initialization
     call MPI_INIT(err)
     call MPI_COMM_SIZE(MPI_COMM_WORLD, nproc, err)
     call MPI_COMM_RANK(MPI_COMM_WORLD, myID, err)
 
     if (nproc /= 4 .AND. myID == 0) then
-        write(*, *) "Программа работает только с 4 потоками"
+        write(*, *) "This program works only in 4 streams"
         call MPI_ABORT(MPI_COMM_WORLD, 0, err)
     end if
     
-    ! чтение матрицы
+    ! matrix reading
     if(myID == 0) then
     
         open(0, file='A')
@@ -30,31 +30,31 @@ program matrixTranspose
             read(0, *) (matrix(i, j), j = 1, columns)
         end do
 
-        write(*, *) "Исходная матрица"
+        write(*, *) "Input matrix"
         call printMatrix(matrix, rows, columns)
 
-        ! если размеры матрицы не чётные будет лажа)
+        ! it works only with even num of rows and columns)
         rows = rows / 2
         columns = columns / 2
     endif
 
-    ! рассылка размерности матрицы
+    ! sending matrix dimensions
     call MPI_BCAST(rows,    1, MPI_INTEGER, 0, MPI_COMM_WORLD, err)
     call MPI_BCAST(columns, 1, MPI_INTEGER, 0, MPI_COMM_WORLD, err)
     
     if(myID == 0) then
-        ! рассылка фрагментов матрицы
+        ! sending matrix parts
         call MPI_SEND(matrix(1:rows,        columns+1:2*columns), rows*columns, MPI_REAL, 1, 0, MPI_COMM_WORLD, err)
         call MPI_SEND(matrix(rows+1:2*rows, 1:columns),           rows*columns, MPI_REAL, 2, 0, MPI_COMM_WORLD, err)
         call MPI_SEND(matrix(rows+1:2*rows, columns+1:2*columns), rows*columns, MPI_REAL, 3, 0, MPI_COMM_WORLD, err)
 	
-        ! транспонирование фрагмента матрицы
+        ! tranposing part of matrix
         call transposeMatrix(matrix(1:rows, 1:columns), rows, columns)
 
     else
         allocate(matrix(rows,columns))
 
-        ! приём фрагмента матрицы, транспонирование и отправка обратно
+        ! recieving matrix part, transposing and sending back
         call MPI_RECV(matrix, rows*columns, MPI_REAL, 0, 0, MPI_COMM_WORLD, status, err)
         call transposeMatrix(matrix, rows, columns)
         call MPI_SEND(matrix, rows*columns, MPI_REAL, 0, 0, MPI_COMM_WORLD, err)
@@ -64,12 +64,12 @@ program matrixTranspose
 
     if(myID == 0) then
 
-        ! приём и сборка транспонированной матрицы
+        ! receiving transposed matrix
         call MPI_RECV(matrix(rows+1:2*rows, 1:columns          ), rows*columns, MPI_REAL, 1, 0, MPI_COMM_WORLD, status, err)
         call MPI_RECV(matrix(1:rows,        columns+1:2*columns), rows*columns, MPI_REAL, 2, 0, MPI_COMM_WORLD, status, err)
         call MPI_RECV(matrix(rows+1:2*rows, columns+1:2*columns), rows*columns, MPI_REAL, 3, 0, MPI_COMM_WORLD, status, err)
         
-        write(*, *) "Транспонированная матрица"
+        write(*, *) "Transposed matrix"
         call printMatrix(matrix, 2*rows, 2*columns)
 
         deallocate(matrix)  
